89dd3c0bed8b4c7b896fe56e118d32f6d36634ba
Deccan Maniam <dev.oofsauce@gmail.com>
2025-06-17 21:25:42 -0400

    dark mode ON

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function load() {
     // fun with timeouts
-    setTimeout(() => {
-        document.getElementById("door").src = "images/door_frames/door_frame2.png";
-    }, 500);
+    document.getElementById("door").style.pointerEvents = "none";
+    document.getElementById("door").src = "images/door_frames/door_frame2.png";
 
     setTimeout(() => {
         document.getElementById("door").src = "images/door_frames/door_frame3.png";
-    }, 1000);
+    }, 500);
 
     setTimeout(() => {
         document.getElementById("page").style.zIndex = 1;
         document.getElementById("space").style.zIndex = 0;
 
         document.getElementById("page").style.animation = "iris ease-out 2s forwards";
-    }, 1500);
+    }, 1000);
 
     setTimeout(() => {
         document.getElementById("pfp").style.visibility = "visible";
         document.getElementById("pfp").style.animation = "appear 0.3s ease-in, shake 0.2s 0.4s steps(1, end)";
-    }, 2500);
+    }, 2000);
 }
--- simple/script.js
+++ simple/script.js
@@ -1,6 +1,71 @@
 let black = false;
+let dark = localStorage.getItem("darkMode") == "true";
+
+window.onload = () => {
+    if(dark) {
+        document.getElementById("bg-dark").style.zIndex = "5";
+        document.getElementById("bg-light").style.zIndex = "0";
+    } else {
+        document.getElementById("bg-dark").style.zIndex = "0";
+        document.getElementById("bg-light").style.zIndex = "5";
+    }
+
+    Array.prototype.forEach.call(document.getElementsByClassName("info"), (element) => {
+        element.style.transform = "translateY(0)";
+        element.style.opacity = "1";
+    });
+
+    for(let i=0; i<5; i++) {
+        setTimeout(() => {
+            Array.prototype.forEach.call(document.getElementsByClassName(`link${i+1}`), (element) => {
+                element.style.visibility = "visible";
+                element.style.opacity = "1";
+            });
+        }, 500 + 100*i);
+    }
+
+    setTimeout(() => {
+        Array.prototype.forEach.call(document.getElementsByClassName("footer"), (element) => {
+            element.style.visibility = "visible";
+            element.style.opacity = "1";
+        });
+    }, 1000);
+}
 
 const loadPage = () => {
     black = !black;
-    document.getElementById("content").style = black ? "background-color: black;" : "background-color: #f0d2c9;";
+    Array.prototype.forEach.call(document.getElementsByClassName("content"), (element) => {
+        element.style = black ? "background-color: black;" : "";
+    });
+}
+
+const toggleDarkMode = () => {
+    localStorage.setItem("darkMode", dark ? "false" : "true");
+
+    let rect = document.getElementById(`toggle-${dark ? "dark" : "light"}`).getBoundingClientRect(); // doesnt matter which, really
+    let x = (rect.left + rect.right) / 2;
+    let y = (rect.top + rect.bottom) / 2;
+
+    document.getElementById("toggle-dark").style.pointerEvents = "none";
+    document.getElementById("toggle-light").style.pointerEvents = "none";
+
+    dark = !dark;
+    let type = dark ? "bg-dark" : "bg-light";
+    let antiType = dark ? "bg-light" : "bg-dark";
+    document.getElementById(type).style.clipPath = `circle(0% at ${x}px ${y}px)`;
+    document.getElementById(type).style.zIndex = "5";
+    document.getElementById(antiType).style.zIndex = "0";
+    document.getElementById(type).animate([
+        { clipPath: `circle(0% at ${x}px ${y}px)` },
+        { clipPath: `circle(200% at ${x}px ${y}px)` }
+    ], {
+        duration: 2500,
+        fill: "forwards",
+        easing: "ease-out"
+    });
+
+    setTimeout(() => {
+        document.getElementById("toggle-dark").style.pointerEvents = "fill";
+        document.getElementById("toggle-light").style.pointerEvents = "fill";
+    }, 2000);
 }
